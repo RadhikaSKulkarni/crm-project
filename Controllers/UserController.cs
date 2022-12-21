ee787a934461e969e92017bb99bb74a81a07ad76
RadhikaSKulkarni <119515729+RadhikaSKulkarni@users.noreply.github.com>
2022-12-22 00:34:12 +0800

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        public UserController() { }
+
+        public string Get()
+        {
+
+            return "HEllo world";
+        }
+
+    }
+}
